d6776cc21feb63dbdc7199fe196b98c64283c0c3
IvanKnyazev <ivan.iv.knyazev@gmail.com>
2017-04-19 16:29:59 +0300

    Initial commit

---
+++ src/Hello_7.java
@@ -0,0 +1,9 @@
+/**
+ * @author Ivan Knyazev on 19.04.2017
+ * @since JDK 1.8
+ */
+public class Hello_7 {
+    public static void main(String[] args) {
+        System.out.println("Hello #7!!!");
+    }
+}
