1b186141a304595e37a62e73f611bc3194e42910
IvanKnyazev <ivan.iv.knyazev@gmail.com>
2017-04-20 09:21:55 +0300

    add first homework: module1

---
+++ src/module1/DZ_01_1.java
@@ -0,0 +1,18 @@
+package module1;
+
+import java.util.Scanner;
+
+/**
+ * @author Ivan Knyazev on 19.04.2017
+ * @since JDK 1.8
+ */
+public class DZ_01_1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+
+        String name = scanner.nextLine();
+        System.out.println("\nHello, " + name);
+    }
+}
---
+++ src/module1/DZ_01_2.java
@@ -0,0 +1,35 @@
+package module1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * @author Ivan Knyazev on 19.04.2017
+ * @since JDK 1.8
+ */
+public class DZ_01_2 {
+    public static void main(String[] args) {
+
+        int count = 1;
+        int size = 5;
+        int[] arrNumber = new int[size];
+        int i;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please, enter five integer numbers\n");
+
+        try {
+            for (i = 0; i < arrNumber.length; i++) {
+                System.out.println("Enter number" + count++ + ": ");
+                arrNumber[i] = scanner.nextInt();
+            }
+            System.out.print("Initial array: " + Arrays.toString(arrNumber));
+        }
+        catch (Exception e){
+            System.out.println("Error, please enter correct number");
+        }
+
+        Arrays.sort(arrNumber);
+        System.out.print("\nSorted array:  " + Arrays.toString(arrNumber));
+    }
+}
